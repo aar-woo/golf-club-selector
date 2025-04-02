00fbf58a5bd3cda5af2943a08c358f6d63103cdf
aar-woo <awooyes@gmail.com>
2025-04-02 14:26:09 -0700

    Refactor so rec club is changed when user releases long press, don't check every single number.

--- golf-club-selector/app/DistanceAndClubView.tsx
+++ golf-club-selector/app/DistanceAndClubView.tsx
@@ -5,6 +5,8 @@ import DistanceView from "@/components/DistanceView/DistanceView";
 
 const DistanceAndClubView = () => {
   const [distance, setDistance] = useState<number>(100);
+  const [displayDistance, setDisplayDistance] = useState(distance);
+  const tempDistanceRef = useRef<number>(distance);
   const counterRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleClickChange = (direction: "left" | "right") => {
@@ -14,9 +16,12 @@ const DistanceAndClubView = () => {
           return;
         }
         setDistance(distance - 1);
+        setDisplayDistance(tempDistanceRef.current);
         break;
       case "right":
         setDistance(distance + 1);
+        tempDistanceRef.current = tempDistanceRef.current + 1;
+        setDisplayDistance(tempDistanceRef.current);
     }
   };
 
@@ -24,24 +29,18 @@ const DistanceAndClubView = () => {
     switch (direction) {
       case "left":
         counterRef.current = setInterval(() => {
-          if (distance <= 0 && counterRef.current) {
+          if (tempDistanceRef.current <= 0 && counterRef.current) {
             clearInterval(counterRef.current);
             return;
           }
-          setDistance((prev) => {
-            if (prev <= 0) {
-              if (counterRef.current) {
-                clearInterval(counterRef.current);
-              }
-              return 0;
-            }
-            return prev - 1;
-          });
+          tempDistanceRef.current = Math.max(0, tempDistanceRef.current - 1);
+          setDisplayDistance(tempDistanceRef.current);
         }, 20);
         break;
       case "right":
         counterRef.current = setInterval(() => {
-          setDistance((prev) => prev + 1);
+          tempDistanceRef.current = tempDistanceRef.current + 1;
+          setDisplayDistance(tempDistanceRef.current);
         }, 20);
     }
   };
@@ -49,13 +48,15 @@ const DistanceAndClubView = () => {
   const handleLongPressOut = () => {
     if (counterRef.current) {
       clearInterval(counterRef.current);
+      setDistance(tempDistanceRef.current);
+      setDisplayDistance(tempDistanceRef.current);
     }
   };
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <RecommendedClubView distance={distance} />
       <DistanceView
-        distance={distance}
+        distance={displayDistance}
         onClickChange={handleClickChange}
         onLongPress={handleLongPressChange}
         onLongPressOut={handleLongPressOut}
